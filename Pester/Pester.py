b7f7b28e128c790923db8269f84000d837927e67
Connor Neff <connor.neff77@gmail.com>
2017-04-14 20:34:13 -0500

    Lesson 1 & 2

--- Pester/Pester.py
+++ Pester/Pester.py
@@ -23,16 +23,52 @@
 #
 #
 ######################################
+##Tkinter Lesson 1
+#from Tkinter import *
+
+
+
+#window = Tk()
+
+#label = Label(window, text="hey")
+
+
+#label.pack()
+
+#window.mainloop()
+
+
+##Tkinter Lesson 2
+
+#from Tkinter import *
+#window = Tk()
+
+#topFrame = Frame(window)
+#topFrame.pack(side=TOP)
+
+#bottomFrame = Frame(window)
+#bottomFrame.pack(side=BOTTOM)
+
+#button1 = Button(topFrame,text="Button1",fg="red")
+#button2 = Button(topFrame,text="Button2",fg="blue")
+#button3 = Button(topFrame,text="Button3",fg="green")
+#button4 = Button(bottomFrame,text="Button4",fg="purple")
+
+
+#button1.pack(side=LEFT)
+#button2.pack(side=LEFT)
+#button3.pack(side=LEFT)
+#button4.pack(side=BOTTOM)
+
+
+#window.mainloop()
+
+##Tkinter Lesson3
 
 from Tkinter import *
-
-
-
 window = Tk()
 
-label = Label(window, text="hey")
 
 
-label.pack()
 
 window.mainloop()
